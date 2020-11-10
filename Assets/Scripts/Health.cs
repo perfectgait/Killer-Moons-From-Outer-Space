cca243c82224188fa9836025a7acbfcd4c70a689
Matt Rathbun <mrathbun80@gmail.com>
2020-11-09 20:33:12 -0700

    Add health script and organize enemies in play space

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health;
+
+    public void Damage(float amount)
+    {
+        health -= amount;
+
+        // Ensure health doesn't drop below zero
+        health = Mathf.Max(0, health);
+
+        if (health <= 0)
+        {
+            Kill();
+        }
+    }
+
+    private void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
