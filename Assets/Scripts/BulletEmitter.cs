8f8ef3be27d3af2460334cfa3aba286a124c541b
Rusty Bailey <rustylbailey@gmail.com>
2020-11-10 22:34:27 -0500

    Add headers and tooltips to clarify how bullet emitter works

--- Assets/Scripts/BulletEmitter.cs
+++ Assets/Scripts/BulletEmitter.cs
@@ -6,16 +6,32 @@ using UnityEngine;
 public class BulletEmitter : MonoBehaviour
 {
     // TODO Group with headings
+    [Header("Prefabs")]
     [SerializeField] GameObject projectilePrefab;
+    [Tooltip("If no origin point is provided, default origin point is this script's game object")]
     [SerializeField] GameObject bulletOriginPoint;
-    [SerializeField] float speed = 8f;
-    [SerializeField] int bulletCountPerLoop = 3;
-    [SerializeField] int bulletsPerCluster = 10;
+
+    [Header("Bullet")]
+    [SerializeField] float bulletSpeed = 8f;
+    [Tooltip("Number of bullets in a wave - set to 1 for a single shot")]
+    [SerializeField] int bulletCountPerWave = 10;
+    [Tooltip("Number of waves to emit before waiting")]
+    [SerializeField] int wavesPerPulse = 3;
+
+    [Header("Arc")]
+    [Tooltip("0 is north and it counts clockwise")]
+    [Range(0, 360)]
     [SerializeField] int startAngle = 90;
+    [Range(0, 360)]
     [SerializeField] int endAngle = 270;
+
+    [Header("Pulse")]
+    [Tooltip("Time between each individual bullet or wave of bullets")]
     [SerializeField] float waitTimeBetweenBullets = .25f;
-    [SerializeField] bool loopBulletCount = true;
-    [SerializeField] float waitTimeBetweenLoop = 2f;
+    [Tooltip("Time between each pulse - set to 0 for a continuous stream")]
+    [SerializeField] float waitTimeBetweenPulse = 0f;
+    [Tooltip("Continue shooting for the lifetime of the game object?")]
+    [SerializeField] bool continousEmission = true;
 
 
     // Start is called before the first frame update
@@ -41,11 +57,11 @@ public class BulletEmitter : MonoBehaviour
 
         do
         {
-            for (var i = 1; i <= bulletCountPerLoop; i++)
+            for (var i = 1; i <= wavesPerPulse; i++)
             {
-                float angleStep = (endAngle - startAngle) / bulletsPerCluster - 1;
+                float angleStep = (endAngle - startAngle) / bulletCountPerWave - 1;
                 float angle = startAngle;
-                for (var j = 1; j <= bulletsPerCluster; j++)
+                for (var j = 1; j <= bulletCountPerWave; j++)
                 {
                     float dirX = bulletOriginTransform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
                     float dirY = bulletOriginTransform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
@@ -54,12 +70,12 @@ public class BulletEmitter : MonoBehaviour
                     Vector3 bulVector = (moveVector - bulletOriginTransform.position).normalized;
 
                     var projectile = Instantiate(projectilePrefab, bulletOriginTransform.position, Quaternion.identity);
-                    projectile.GetComponent<Projectile>().SetVelocity(speed, bulVector);
+                    projectile.GetComponent<Projectile>().SetVelocity(bulletSpeed, bulVector);
                     angle += angleStep;
                 }
                 yield return new WaitForSeconds(waitTimeBetweenBullets);
             }
-            yield return new WaitForSeconds(waitTimeBetweenLoop);
-        } while (loopBulletCount);
+            yield return new WaitForSeconds(waitTimeBetweenPulse);
+        } while (continousEmission);
     }
 }
