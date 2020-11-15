a5392cc5c6972e3041290bab05f6232deebd441a
Rusty Bailey <rustylbailey@gmail.com>
2020-11-15 18:02:17 -0500

    Add flashing effects when player/enemy is damaged

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -10,12 +10,18 @@ public class Health : MonoBehaviour
 
     IFrames iframes;
     AudioManager audioManager;
+    SpriteRenderer spriteRenderer;
+
+    IEnumerator flashingCoroutine;
 
     // Start is called before the first frame update
     void Start()
     {
         iframes = GetComponent<IFrames>();
         audioManager = AudioManager.instance;
+        // GetComponentInChildren finds the first component in the parent or child
+        // so this works for both the player's body and the enemies
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     public void Damage(float amount)
@@ -41,13 +47,37 @@ public class Health : MonoBehaviour
         }
         else
         {
-            audioManager.PlaySoundEffect("Damage Hit");
+            HandleDamageEffects();
         }
     }
 
     public float GetHealth()
     {
         return health;
+    }
+
+    private void HandleDamageEffects()
+    {
+        audioManager.PlaySoundEffect("Damage Hit");
+        if (flashingCoroutine != null)
+        {
+            StopCoroutine(flashingCoroutine);
+        }
+        flashingCoroutine = Flash(2);
+        StartCoroutine(flashingCoroutine);
+    }
+
+    private IEnumerator Flash(int numTimes)
+    {
+        bool makeSpriteWhite = true;
+
+        // Iterate twice the length of times - that way numTimes is "how many times is it turned on", not "how many times does it flip".
+        for (int i = 0; i < numTimes * 2; i++)
+        {
+            GetComponentInChildren<SpriteRenderer>().material.SetFloat("_FlashAmount", makeSpriteWhite ? 1f : 0f);
+            makeSpriteWhite = !makeSpriteWhite;
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 
     private void Kill()
