773b57572c0c6dca0c8022809993e00cfc783454
Matt Rathbun <mrathbun80@gmail.com>
2020-11-21 23:08:12 -0700

    Start working on moon guns

--- Assets/Scripts/BulletEmitter.cs
+++ Assets/Scripts/BulletEmitter.cs
@@ -44,7 +44,7 @@ public class BulletEmitter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        audioManager = AudioManager.instance;
+        //audioManager = AudioManager.instance;
     }
 
     // Update is called once per frame
@@ -71,7 +71,7 @@ public class BulletEmitter : MonoBehaviour
         {
             for (var i = 1; i <= wavesPerPulse; i++)
             {
-                audioManager.PlaySoundEffect(soundEffectName);
+                //audioManager.PlaySoundEffect(soundEffectName);
 
                 int randomAngleVariance = Random.Range(-angleVariance, angleVariance);
                 float angle = startAngle + randomAngleVariance;
