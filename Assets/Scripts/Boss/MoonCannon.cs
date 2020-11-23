a22a6a2bd6e9b172567840b34926631647e10c65
Matt Rathbun <mrathbun80@gmail.com>
2020-11-23 16:16:34 -0700

    Control player via physics

--- Assets/Scripts/Boss/MoonCannon.cs
+++ Assets/Scripts/Boss/MoonCannon.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class MoonCannon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void StartFiring()
     {
         BulletEmitter bulletEmitter = GetComponent<BulletEmitter>();
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,27 +6,16 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float horizontalMovementSpeed = 7.0f;
     [SerializeField] float verticalMovementSpeed = 7.0f;
-    [SerializeField] float xRightPadding = 0.5f;
-    [SerializeField] float xLeftPadding = 0.5f;
-    [SerializeField] float yTopPadding = 0.5f;
-    [SerializeField] float yBottomPadding = 0.5f;
 
     private Coroutine firingCoroutine;
     private BulletEmitter bulletEmitter;
     private Health health;
-    private float xMin;
-    private float xMax;
-    private float yMin;
-    private float yMax;
     private bool isFiring = false;
     private bool canFire = true;
     private float canFireCountdown = 0.0f;
     // Offset is used so a human can fire at the same rate as the computer if they chose to rapid press the fire button vs. hold it down
     private float waitTimeBetweenBulletsOffset = 0.2f;
-    private bool canMove = true;
-    private bool hitWall = false;
-    private Vector3 lastKnownGoodPosition;
-    private Vector3 originalPosition;
+    private Rigidbody2D rigidBody;
 
     // Just used for testing the pacing of the levels
     [SerializeField] bool startWithMinigun = false;
@@ -34,10 +23,9 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SetupMovementBoundaries();
         bulletEmitter = GetComponent<BulletEmitter>();
         health = GetComponent<Health>();
-        originalPosition = transform.position;
+        rigidBody = GetComponent<Rigidbody2D>();
 
         if (startWithMinigun)
         {
@@ -54,38 +42,11 @@ public class Player : MonoBehaviour
         Fire();
     }
 
-    private void SetupMovementBoundaries()
-    {
-        Camera gameCamera = Camera.main;
-
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + xLeftPadding;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - xRightPadding;
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + yBottomPadding;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - yTopPadding;
-    }
-
     private void Move()
     {
-        // If the player hit a wall, move them back to the last known good
-        // position and remove the hit wall flag so they can move again.
-        if (hitWall)
-        {
-            Debug.Log("Resetting after hitting wall");
+        Vector2 newVelocity = new Vector2(Input.GetAxis("Horizontal") * horizontalMovementSpeed, Input.GetAxis("Vertical") * verticalMovementSpeed);
 
-            //transform.position = lastKnownGoodPosition;
-            transform.position = originalPosition;
-            hitWall = false;
-
-            return;
-        }
-
-        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * horizontalMovementSpeed;
-        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * verticalMovementSpeed;
-        float newXPosition = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
-        float newYPosition = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
-
-        transform.position = new Vector3(newXPosition, newYPosition, transform.position.z);
-        lastKnownGoodPosition = transform.position;
+        rigidBody.velocity = newVelocity;
     }
 
     private void Fire()
@@ -185,32 +146,4 @@ public class Player : MonoBehaviour
 
         return 1.0f;
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Wall")
-        {
-            hitWall = true;
-            //transform.position = lastKnownGoodPosition;
-
-            Debug.Log("hit wall");
-
-            //canMove = false;
-        }
-    }
-
-    //private void OnTriggerStay2D(Collider2D collision)
-    //{
-    //    if (collision.gameObject.tag == "Wall")
-    //    {
-    //        Debug.Log("stayed in wall");
-
-    //        canMove = false;
-    //    }
-    //}
-
-    //private void OnTriggerExit2D(Collider2D collision)
-    //{
-    //    canMove = true;
-    //}
 }
